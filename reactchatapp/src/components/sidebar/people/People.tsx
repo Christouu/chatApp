aaab8a6c97739a14278b7ac726a9002ac05b5851
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-09-06 14:11:15 +0300

    added search a user functionality

--- reactchatapp/src/components/sidebar/people/People.tsx
+++ reactchatapp/src/components/sidebar/people/People.tsx
@@ -1,63 +1,67 @@
+import {
+  doc,
+  getDoc,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { useContext } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+import { db } from "../../../firebase";
 import "./People.scss";
 
-const People = () => {
+const People = ({ user, setUser, setUsername }: any) => {
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        //create a chat in chat collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            dispalyName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(), //dont use Date.now(). serverTImestamps calculates time zones
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            dispalyName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(), //dont use Date.now(). serverTImestamps calculates time zones
+        });
+      }
+    } catch (error) {}
+
+    setUser(null);
+    setUsername("");
+  };
+
   return (
     <div className="people">
-      <div className="userChat">
-        <img
-          src="https://i2-prod.football.london/incoming/article24928546.ece/ALTERNATES/s1200/2_Mikel-Arteta-and-Arsenal.jpg"
-          alt="user avatar"
-        />
-        <div className="userChatInfo">
-          <span>Arteta</span>
-          <p>brato arsenla sa mega slabi plz vzemi me pri teb</p>
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="user avatar" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+            <p>brato arsenla sa mega slabi plz vzemi me pri teb</p>
+          </div>
         </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://i.insider.com/5e7b5e1eba85ee6bf165c366?width=700"
-          alt="user avatar"
-        />
-        <div className="userChatInfo">
-          <span>Klop</span>
-          <p>trevata beshe tvurde zelena za tva ni bihte , vurzani</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://cdn.resfu.com/media/img_news/afp_fr_a5b3a94d59e1c8dd8e8f67cb9f2ae22ecec85854.jpg?size=1000x&lossy=1"
-          alt="user avatar"
-        />
-        <div className="userChatInfo">
-          <span>Pep</span>
-          <p>
-            s arabite lapame shisha i mi davat pak 1 milqrd da si cukam fifa
-          </p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://i.guim.co.uk/img/media/8a9217fbc9fa1eb2314041e9a223677fb9b9d578/0_62_4134_2481/master/4134.jpg?width=620&quality=85&fit=max&s=6ea579428eeacda955150b03af338513"
-          alt="user avatar"
-        />
-        <div className="userChatInfo">
-          <span>Conte</span>
-          <p>tyka bace kabineta s trofei ima parcali samo</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://i2-prod.mirror.co.uk/incoming/article26440643.ece/ALTERNATES/s615b/0_GettyImages-1238956686.jpg"
-          alt="user avatar"
-        />
-        <div className="userChatInfo">
-          <span>Tuchel</span>
-          <p>
-            100 miliona za Harito davam, mnogo e div iskam go plz, ko she kaish
-            praim li go ?
-          </p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- reactchatapp/src/components/sidebar/search/Search.tsx
+++ reactchatapp/src/components/sidebar/search/Search.tsx
@@ -1,13 +1,50 @@
-import People from "../people/People";
 import "./Search.scss";
+import { useState } from "react";
+import People from "../people/People";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        //@ts-ignore
+        setUser(doc.data());
+      });
+    } catch (error) {
+      //@ts-ignore
+      setError(error);
+    }
+  };
+
+  const handleKey = (e: any) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a user" />
+        <input
+          type="text"
+          placeholder="Find a user"
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
+          value={username}
+        />
       </div>
-      <People />
+      {error && <span>User not found!</span>}
+      <People user={user} setUser={setUser} setUsername={setUsername} />
     </div>
   );
 };
