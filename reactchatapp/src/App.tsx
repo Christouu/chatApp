90126b846b5c0ac0f1f3145d0b7d7b31991d74a0
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-09-06 11:58:17 +0300

    starting logic funcionality with firebase

--- reactchatapp/src/App.tsx
+++ reactchatapp/src/App.tsx
@@ -2,18 +2,32 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
 
+  const ProtectedRoute = ({ children }: any) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route
+            index
+            element={
+              //@ts-ignore
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
         </Route>
